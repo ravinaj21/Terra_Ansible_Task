034a6097963067b8f2ef3636ba1fcdc9e6ce8120
ravinaj21 <115338633+ravinaj21@users.noreply.github.com>
2022-12-28 14:36:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,5 +12,13 @@ terraform {
 provider "azurerm" {
   features {}
   use_msi = true
+  
+  backend "azurerm" {
+     storage_account_name = "terratask"
+     container_name ="terracontainer"
+     key ="infrax.tfstate"
+     subscription_id ="eb610e94-b4e6-450e-863a-f5f04cba5f8d"
+     tenant_id ="66a1b718-3e00-4617-a869-71aa190d2785"
+  }
 }
 
